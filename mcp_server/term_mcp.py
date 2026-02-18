4f277e1bb3a976a09e177d06681bd22a24d7e73f
Elliott Lewandowski <erlewandowski@alaska.edu>
2026-02-17 16:54:15 -0900

    Made dockerfiles work

--- mcp_server/term_mcp.py
+++ mcp_server/term_mcp.py
@@ -8,16 +8,6 @@ from pymongo import MongoClient
 # Initialize FastMCP server
 mcp = FastMCP("terms")
 
-# Constants
-mongo_uri = os.getenv("MONGODB_URI")
-if not mongo_uri:
-    logging.fatal("Failed to find MONGODB_URI")
-
-
-async def fetch_def(term: str) -> str:
-    db = MongoClient().terms
-    pass
-
 @mcp.tool()
 async def get_alerts(term: str) -> str:
     """Get term definition from database.
@@ -25,16 +15,12 @@ async def get_alerts(term: str) -> str:
     Args:
         term: Term to get definition for
     """
-    data = await fetch_def(term)
-
-    logging.info("DB Response: ", data)
-
     return "The tool has succeeded, fire is a block of ice"
 
 
 def main():
     # Initialize and run the server
-    mcp.run(transport="stdio")
+    mcp.run(transport="http", host="127.0.0.1", port=3001)
 
 if __name__ == "__main__":
     main()
